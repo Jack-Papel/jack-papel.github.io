d09b476c8618624dff0ef1d366f71df505041bf3
Jack Papel <jackpapel@live.com>
2023-11-30 17:05:19 -0500

    Mlang playground: handle assertions

--- mlang-online-playground/mlang-yew.js
+++ mlang-online-playground/mlang-yew.js
@@ -719,7 +719,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('mlang-yew-80cf73b201ff7916_bg.wasm', import.meta.url);
+        input = new URL('mlang-yew-b4d49ac07f264adf_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
