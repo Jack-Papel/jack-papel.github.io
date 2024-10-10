560039da193f22e08d8a081cc86052d2bcd1c142
Jack Papel <jackpapel@live.com>
2024-10-10 17:17:55 -0400

    Fix navbar for mlang playground

--- mlang-online-playground/mlang-yew.js
+++ mlang-online-playground/mlang-yew.js
@@ -722,7 +722,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('mlang-yew-f86c6417aab1a2f5_bg.wasm', import.meta.url);
+        input = new URL('mlang-yew-4c764108292c0e29_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
